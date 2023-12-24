26149dd2bdc5a507181a83926d84f13720659015
BastienKody <basguilla@gmail.com>
2023-12-24 15:28:06 +0100

    fix: quit if bad argc vs infinite loop

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -18,7 +18,7 @@ int	open_config_file(int argc, char **argv)
 	const char	*path = argv[1];
 
 	if (argc != 2)
-		return (print_error(BAD_ARG_NB, NULL), 0);
+		return (print_error(BAD_ARG_NB, NULL), -1);
 	path = argv[1];
 	if (ft_strlen(path) < 5 || ft_strcmp(&path[ft_strlen(path) -4], ".cub"))
 		return (print_error(BAD_CONFIG_NAME, NULL), -2);
