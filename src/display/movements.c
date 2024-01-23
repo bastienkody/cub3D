b22808b8960d8a00d8b697598ca1f224a867f529
Bastien Guillaume <bguillau@student.42.fr>
2024-01-23 12:58:00 +0100

    movements okok

--- src/display/movements.c
+++ src/display/movements.c
@@ -12,68 +12,79 @@
 
 #include "../../inc/cub3D.h"
 
-int	is_floor(double newx, double newy, t_info *info)
+/*	mod via player dir	*/
+int	move_front_back_wards(int keycode, t_info *info)
 {
-	if (info->dirx >= 0)
-		newx = ceil(newx);
-	else
-		newx = floor(newx);
-	if (info->diry >= 0)
-		newy = ceil(newy);
-	else
-		newy = floor(newy);
-	printf("is floor(map[%0.f][%0.f])=%i\n", newy, newx, info->map[(int)newy][(int)newx] - '0');
-	return ((info->map[(int)newy][(int)newx] == '0'));
-}
-
-void key_movement(int keycode, t_info *info)
-{
-	const double	xmax = info->mw - 1;
-	const double	ymax = info->mh - 1;
-	double			xofs;
-	double			yofs;
+	double	x_offset;
+	double	y_offset;
 
 	if (keycode == XK_w)
 	{
-		xofs = info->posx + info->dirx * VELO_M;
-		yofs = info->posy + info->diry * VELO_M;
-		if (xofs >= 0 && xofs <= xmax && yofs >= 0 && yofs <= ymax && is_floor(xofs, yofs, info))
+		x_offset = info->posx + info->dirx * VELO_M;
+		y_offset = info->posy + info->diry * VELO_M;
+		if (info->map[(int)floor(y_offset)][(int)floor(x_offset)] == '0')
 		{
-			info->posx = xofs;
-			info->posy = yofs;
+			info->posx = x_offset;
+			info->posy = y_offset;
+			return (1);
 		}
 	}
 	else if (keycode == XK_s)
 	{
-		xofs = info->posx - info->dirx * VELO_M;
-		yofs = info->posy - info->diry * VELO_M;
-		if (xofs >= 0 && xofs < xmax && yofs >= 0 && yofs < ymax  && is_floor(xofs, yofs, info))
+		x_offset = info->posx - info->dirx * VELO_M;
+		y_offset = info->posy - info->diry * VELO_M;
+		if (info->map[(int)floor(y_offset)][(int)floor(x_offset)] == '0')
 		{
-			info->posx = xofs;
-			info->posy = yofs;
+			info->posx = x_offset;
+			info->posy = y_offset;
+			return (1);
 		}
 	}
-	else if (keycode == XK_a)
+	return (0);
+}
+
+/*	mod via player plane (always perpendiculaire to player dir)	*/
+int	move_sides(int keycode, t_info *info)
+{
+	double	x_offset;
+	double	y_offset;
+
+	if (keycode == XK_a)
 	{
-		xofs = info->posx - info->planex * VELO_M;
-		yofs = info->posy - info->planey * VELO_M;
-		if (xofs >= 0 && xofs < xmax && yofs >= 0 && yofs < ymax && is_floor(xofs, yofs, info))
+		x_offset = info->posx - info->planex * VELO_M;
+		y_offset = info->posy - info->planey * VELO_M;
+		if (info->map[(int)floor(y_offset)][(int)floor(x_offset)] == '0')
 		{
-			info->posx = xofs;
-			info->posy = yofs;
+			info->posx = x_offset;
+			info->posy = y_offset;
+			return (1);
 		}
 	}
 	else if (keycode == XK_d)
 	{
-		xofs = info->posx + info->planex * VELO_M;
-		yofs = info->posy + info->planey * VELO_M;
-		if (xofs >= 0 && xofs < xmax && yofs >= 0 && yofs < ymax && is_floor(xofs, yofs, info))
+		x_offset = info->posx + info->planex * VELO_M;
+		y_offset = info->posy + info->planey * VELO_M;
+		if (info->map[(int)floor(y_offset)][(int)floor(x_offset)] == '0')
 		{
-			info->posx = xofs;
-			info->posy = yofs;
+			info->posx = x_offset;
+			info->posy = y_offset;
+			return (1);
 		}
 	}
+	return (0);
+}
+
+/*	calls raycast only if player has moved	*/
+void key_movement(int keycode, t_info *info)
+{
+	bool	redraw;
+
+	redraw = false;
+	if (keycode == XK_w || keycode == XK_s)
+		redraw = move_front_back_wards(keycode, info);
 	else
+		redraw = move_sides(keycode, info);
+	if (!redraw)
 		return ;
 	raycast_launcher(info);
 	draw_minimap(info);
